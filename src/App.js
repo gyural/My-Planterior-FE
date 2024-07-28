e3fa14f8a5d685ddd24ff7880255a538821767f1
Lim, gyuseong <100032171+gyural@users.noreply.github.com>
2024-07-28 21:39:11 +0900

    feat:front-배포-성공

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import { useRecoilValue } from 'recoil';
 import MyPlant from './pages/MyPlant';
 import About from './pages/About';
 import Contact from './pages/Contact';
-import BottomNavBar from './components/BottomNavbar';
 import Header from './components/Header';
 import Recommend from './pages/Recommend';
 import LoginModal from './components/LoginModal';
